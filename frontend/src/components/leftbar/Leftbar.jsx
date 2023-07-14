1c7aa7dd59f11ae285ea4beb499dbac887202a77
stivex001 <stephenadeyemo@gmail.com>
2023-07-14 22:55:00 +0100

    feat:implemeted the leftbar component

--- frontend/src/components/leftbar/Leftbar.jsx
+++ frontend/src/components/leftbar/Leftbar.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { firstData, secondData, thirdData } from "./data";
+import "./leftbar.scss";
 
 const Leftbar = () => {
   return (
-    <div>Leftbar</div>
-  )
-}
+    <div className="leftbar">
+      <div className="wrapper">
+        <div className="menu">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+            <span>Stephen Adeyemo</span>
+          </div>
+          {firstData.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.img} alt="" />
+              <span>{item.desc}</span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Your shortcuts</span>
+          {secondData.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.img} alt="" />
+              <span>{item.desc}</span>
+            </div>
+          ))}
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Others</span>
+          {thirdData.map((item) => (
+            <div className="item" key={item.id}>
+              <img src={item.img} alt="" />
+              <span>{item.desc}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Leftbar
+
+export default Leftbar;
---
+++ frontend/src/components/leftbar/data.js
@@ -0,0 +1,87 @@
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Gallery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Tutorials from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import Fund from "../../assets/13.png";
+
+export const firstData = [
+  {
+    id: 1,
+    img: Friends,
+    desc: "Friends",
+  },
+  {
+    id: 2,
+    img: Groups,
+    desc: "Groups",
+  },
+  {
+    id: 3,
+    img: Market,
+    desc: "Marketplace",
+  },
+  {
+    id: 4,
+    img: Watch,
+    desc: "Watch",
+  },
+  {
+    id: 5,
+    img: Memories,
+    desc: "Memories",
+  },
+];
+
+export const secondData = [
+  {
+    id: 1,
+    img: Events,
+    desc: "Events",
+  },
+  {
+    id: 2,
+    img: Gaming,
+    desc: "Gaming",
+  },
+  {
+    id: 3,
+    img: Gallery,
+    desc: "Gallery",
+  },
+  {
+    id: 4,
+    img: Videos,
+    desc: "Videos",
+  },
+  {
+    id: 5,
+    img: Messages,
+    desc: "Messages",
+  },
+];
+
+export const thirdData = [
+  {
+    id: 1,
+    img: Fund,
+    desc: "Fundraiser",
+  },
+  {
+    id: 2,
+    img: Tutorials,
+    desc: "Tutorials",
+  },
+  {
+    id: 3,
+    img: Courses,
+    desc: "Courses",
+  },
+];
