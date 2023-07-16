6a2a53a11f27575de5f824c83d86a49d3470a0e2
stivex001 <stephenadeyemo@gmail.com>
2023-07-17 00:23:21 +0100

    implemented the controller functions

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
-import  express  from "express";
+import express from "express";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import likesRoutes from "./routes/likes.js";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
+// Routes
+
+app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/comments", commentRoutes);
+// app.use("/api/v1/posts", postRoutes);
+// app.use("/api/v1/likes", likesRoutes);
+app.use("/api/v1/auth", authRoutes);
+
 app.listen(8080, () => {
-    try {
-        console.log("connected to port 8080");
-    } catch (error) {
-        if (error) throw error;
-    }
-    
-})
+  try {
+    console.log("connected to port 8080");
+  } catch (error) {
+    if (error) throw error;
+  }
+});
