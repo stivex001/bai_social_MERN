cbf7c2775e3f8ff380a449211be5a136967a5c39
stivex001 <stephenadeyemo@gmail.com>
2023-07-17 00:41:40 +0100

    added bcrypt and detenv package

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,7 +1,25 @@
+import { db } from "../db/connect.js";
+
+export const register = async (req, res) => {
+  try {
+    // check if user exists
+
+    const q = "SELECT FROM users WHERE username = ?";
+
+    db.query(q, [req.body.username], (err, data) => {
+      if (err) return res.status(500).json(err);
+      if (data.length) return res.status(409).json("User already exists")
+    });
+    
+  } catch (error) {}
+};
+
 export const login = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  try {
+  } catch (error) {}
+};
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {}
+};
