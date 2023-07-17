51631ae16180d363e06bd35e62861c6dd9d02766
stivex001 <stephenadeyemo@gmail.com>
2023-07-17 12:33:23 +0100

    implemeted logout function

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,6 @@
 import { db } from "../db/connect.js";
 import bcrypt from "bcryptjs";
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // check if user exists
@@ -31,32 +31,41 @@ export const register = (req, res) => {
   });
 };
 
-export const login =  (req, res) => {
-  const {username, password} = req.body
+export const login = (req, res) => {
+  const { username, password } = req.body;
 
   // check if the user exists
-  const q = "SELECT * FROM users WHERE username = ?"
-  db.query(q, [username], (err, data) =>{
-    if(err) return res.status(500).json(err)
+  const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [username], (err, data) => {
+    if (err) return res.status(500).json(err);
 
-    if(data.length === 0) return res.status(404).json("User not found")
+    if (data.length === 0) return res.status(404).json("User not found");
 
     // validate the password
-    const correctPassword = bcrypt.compareSync(password, data[0].password)
+    const correctPassword = bcrypt.compareSync(password, data[0].password);
 
-    if(!correctPassword) return res.status(400).json("Wrong password or username")
+    if (!correctPassword)
+      return res.status(400).json("Wrong password or username");
 
-    const token = jwt.sign({id: data[0].id}, 'mysecret')
+    const token = jwt.sign({ id: data[0].id }, "mysecret");
 
-    const {password: _, ...others} = data[0]
+    const { password: _, ...others } = data[0];
 
-    res.cookie("access_token", token, {
-      httpOny: true,
-    }).status(200).json(others)
-  })
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  });
 };
 
 export const logout = async (req, res) => {
-  try {
-  } catch (error) {}
+  res
+    .cookie("access_token", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .json("User logged out successfully");
 };
