4dbab8d4817282312c6cb1eaa6f1a3d0cdcdd97a
stivex001 <stephenadeyemo@gmail.com>
2023-07-17 22:48:12 +0100

    feat:added onchange events to the form inputs

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,7 +1,20 @@
 import { Link } from "react-router-dom";
 import "./register.scss";
+import { useState } from "react";
 
 const Register = () => {
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: ""
+  })
+
+  const handleChange = (e) => {
+    setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+  }
+
   return (
     <div className="register">
       <div className="wrapper">
@@ -19,10 +32,10 @@ const Register = () => {
         <div className="right">
           <h1>Register</h1>
           <form action="">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" onChange={handleChange} name="name" />
+            <input type="text" placeholder="Username" onChange={handleChange} name="username" />
+            <input type="email" placeholder="Email" onChange={handleChange} name="email" />
+            <input type="password" placeholder="Password" onChange={handleChange} name="password" />
             <button>Register</button>
           </form>
         </div>
