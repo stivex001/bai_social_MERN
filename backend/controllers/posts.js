884dc395ca3182864555374198981abadc5788a9
stivex001 <stephenadeyemo@gmail.com>
2023-07-18 20:22:15 +0100

    feat:queried user,postand relationship tables

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,10 +1,19 @@
 import { db } from "../db/connect.js";
+import jwt from "jsonwebtoken";
 
 export const getPosts = (req, res) => {
-  const q = `SELECT p.*, u.id AS userId, name, profilePicture FROM posts AS p JOIN users AS u ON (u.id = p.userId)`;
+  const token = req.cookies.access_token;
+  if (!token) return res.status(403).json("Not Authorized");
 
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json(err);
-    return res.status(200).json(data);
+  jwt.verify(token, "mysecret", (err, userInfo) => {
+    if (err) return res.status(403).json("Invalid Token");
+
+    const q = `SELECT p.*, u.id AS userId, name, profilePicture FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
+     LEFT JOIN relationships AS r ON (p.userId = r.followedUserid ) WHERE r.followerUserid = ? OR p.userId = ?`;
+
+    db.query(q, [userInfo.id, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json(data);
+    });
   });
 };
