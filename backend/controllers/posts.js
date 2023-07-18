46b502cadd270eb6c366815dc737fbd7b60114c6
stivex001 <stephenadeyemo@gmail.com>
2023-07-18 20:34:25 +0100

    feat:added moment package to both api and client

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -9,7 +9,7 @@ export const getPosts = (req, res) => {
     if (err) return res.status(403).json("Invalid Token");
 
     const q = `SELECT p.*, u.id AS userId, name, profilePicture FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
-     LEFT JOIN relationships AS r ON (p.userId = r.followedUserid ) WHERE r.followerUserid = ? OR p.userId = ?`;
+     LEFT JOIN relationships AS r ON (p.userId = r.followedUserid ) WHERE r.followerUserid = ? OR p.userId = ? ORDER BY p.created_at DESC`;
 
     db.query(q, [userInfo.id, userInfo.id], (err, data) => {
       if (err) return res.status(500).json(err);
