d557491dd56072f3fbba8d339944eff5c8ca8ebb
stivex001 <stephenadeyemo@gmail.com>
2023-07-22 12:05:11 +0100

    fix: typo fixed

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,16 +1,26 @@
-import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsThreeDotsVertical, BsTwitter } from "react-icons/bs";
+import {
+  BsFacebook,
+  BsGithub,
+  BsInstagram,
+  BsLinkedin,
+  BsThreeDotsVertical,
+  BsTwitter,
+} from "react-icons/bs";
 import "./profile.scss";
 import { MdLanguage, MdOutlineMailOutline, MdPlace } from "react-icons/md";
 import Posts from "../../components/posts/Posts";
 import { useQuery } from "react-query";
 import { apiCalls } from "../../axios";
 import { useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 
 const Profile = () => {
+  const userId = parseInt(useLocation().pathname.split("/")[2]);
 
-  const userId = useLocation().pathname.split("/")[2]
+  const { currentUser } = useContext(AuthContext);
 
-  const { isLoading, error, data } = useQuery(["user"], () =>
+  const { isLoading, error, data } = useQuery(["users"], () =>
     apiCalls.get("/users/find/" + userId).then((res) => {
       return res.data;
     })
@@ -20,57 +30,62 @@ const Profile = () => {
 
   return (
     <div className="profile">
-      <div className="images">
-        <img
-          src={data.coverPic}
-          alt=""
-          className="cover"
-        />
-        <img
-          src={data.profilePicture}
-          className="profilePic"
-        />
-      </div>
-      <div className="profileContainer">
-        <div className="userInfo">
-          <div className="left">
-            <a href="https://web.facebook.com/">
-              <BsFacebook size={30} />
-            </a>
-            <a href="https://web.facebook.com/">
-              <BsInstagram size={30} />
-            </a>
-            <a href="https://www.linkedin.com/in/adeyemostephen/">
-              <BsLinkedin size={30} />
-            </a>
-            <a href="https://twitter.com/Baistevoo">
-              <BsTwitter size={30} />
-            </a>
-            <a href="https://github.com/stivex001">
-              <BsGithub size={30} />
-            </a>
+      {error ? (
+        <div style={{ color: "red" }}>Opps!! Something Went Wrong!</div>
+      ) : isLoading ? (
+        <div style={{ color: "green", fontSize: "30px" }}>Loading.........</div>
+      ) : (
+        <>
+          <div className="images">
+            <img src={data.coverPic} alt="" className="cover" />
+            <img src={data.profilePicture} className="profilePic" />
           </div>
-          <div className="center">
-            <span>{data.name}</span>
-            <div className="info">
-              <div className="item">
-                <MdPlace />
-                <span>{data.city}</span>
+          <div className="profileContainer">
+            <div className="userInfo">
+              <div className="left">
+                <a href="https://web.facebook.com/">
+                  <BsFacebook size={30} />
+                </a>
+                <a href="https://web.facebook.com/">
+                  <BsInstagram size={30} />
+                </a>
+                <a href="https://www.linkedin.com/in/adeyemostephen/">
+                  <BsLinkedin size={30} />
+                </a>
+                <a href="https://twitter.com/Baistevoo">
+                  <BsTwitter size={30} />
+                </a>
+                <a href="https://github.com/stivex001">
+                  <BsGithub size={30} />
+                </a>
               </div>
-              <div className="item">
-                <MdLanguage />
-                <span>{data.website} </span>
+              <div className="center">
+                <span>{data.name}</span>
+                <div className="info">
+                  <div className="item">
+                    <MdPlace />
+                    <span>{data.city}</span>
+                  </div>
+                  <div className="item">
+                    <MdLanguage />
+                    <span>{data.website} </span>
+                  </div>
+                </div>
+                {userId === currentUser.id ? (
+                  <button>Update</button>
+                ) : (
+                  <button>follow</button>
+                )}
+              </div>
+              <div className="right">
+                <MdOutlineMailOutline />
+                <BsThreeDotsVertical />
               </div>
             </div>
-            <button>follow</button>
+            <Posts />
           </div>
-          <div className="right">
-            <MdOutlineMailOutline />
-            <BsThreeDotsVertical />
-          </div>
-        </div>
-        <Posts />
-      </div>
+        </>
+      )}
     </div>
   );
 };
